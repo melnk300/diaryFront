588d8567debdb3ee289a0bb1d7a44aaa2d6565f0
melnk300 <melnk300@gmail.com>
2021-02-07 23:27:41 +0500

    Init commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,15 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <span class="link_n"><router-link to="/">Home</router-link></span> |
+      <span class="link_n"><router-link to="/about">Reg</router-link></span>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  .link_n {
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
-}
 </style>
---
+++ src/views/RegisterUser.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main">
+    <h2 class="greetings">Sign up to Diary</h2>
+    <h5 class="have_account">Already have an account? <a href="#" class="link_d">Log in</a></h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RegisterUser",
+  data: function () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
