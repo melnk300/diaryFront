ee45d18a14e3d37871e4b0e2b2d609967f315b7f
melnk300 <melnk300@gmail.com>
2021-02-07 23:44:47 +0500

    Added inputs

--- src/views/RegisterUser.vue
+++ src/views/RegisterUser.vue
@@ -2,6 +2,12 @@
   <div class="main">
     <h2 class="greetings">Sign up to Diary</h2>
     <h5 class="have_account">Already have an account? <a href="#" class="link_d">Log in</a></h5>
+    <form>
+      <label for="email">Email:</label>
+      <input id="email" type="email" placeholder="example@mail.com">
+      <label for="password">Password:</label>
+      <input id="password" type="password">
+    </form>
   </div>
 </template>
 
@@ -10,7 +16,8 @@ export default {
   name: "RegisterUser",
   data: function () {
     return {
-
+      email: '',
+      password: ''
     }
   }
 }
